e0ed3ae8d8e4e4363bf77d8abb6f8b7e6780512e
Victor Lopez <victor.lopez@keybotic.com>
2022-11-18 13:27:57 +0100

    Stop power board frontend before flashing

--- scripts/flash_teensy_40.sh
+++ scripts/flash_teensy_40.sh
@@ -5,6 +5,12 @@ if [ $# -ne 1 ]; then
   exit 1
 fi
 
+echo "Attempting to kill power_board_frontend"
+ros2 run key_startup key_app_stop.sh power_board_frontend
+
 hex_file=$1
 
 ros2 run teensy_loader_cli teensy_loader_cli -v --mcu=TEENSY40 -s $hex_file
+
+echo "Attempting to kill power_board_frontend"
+ros2 run key_startup key_app_start.sh power_board_frontend
